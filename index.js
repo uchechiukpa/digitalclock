22340847c2b12cf1f242087e3a3717820d20e864
uchechi <uchechi.ukpa@globalaccelerex.com>
2023-07-24 17:50:07 +0100

    created a digital clock

---
+++ index.js
@@ -0,0 +1,25 @@
+function getTime() {
+  let date = new Date();
+  let hour = date.getHours();
+  let minute = date.getMinutes();
+  let seconds = date.getSeconds();
+
+  let session = "PM";
+
+  if (hour == 0) {
+    hour = 12;
+    session = "AM";
+  }
+  if (hour <= 12) {
+    session = "PM";
+    if (hour > 12) hour = hour - 12;
+  }
+
+  hour = hour < 10 ? "0" + hour : hour;
+  minute = minute < 10 ? "0" + minute : minute;
+  seconds = seconds < 10 ? "0" + seconds : seconds;
+  let time = hour + ":" + minute + ":" + seconds + " " + session;
+  document.getElementById("digitalclock").innerText = time;
+}
+getTime();
+setInterval(getTime, 1000);
